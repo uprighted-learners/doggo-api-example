2b08e8213a8ce8218d54e4a4f4a82243d16d1e21
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-21 18:59:38 -0500

    added async function

--- script.js
+++ script.js
@@ -2,20 +2,41 @@ const dogImageContainer = document.getElementById('dog-image')
 const newDogBtn = document.getElementById('new-dog-btn')
 
 // function for a random dog image
-function getRandomDog() {
+// function getRandomDog() {
+//     const url = 'https://dog.ceo/api/breeds/image/random'
+
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             const dogImg = document.createElement('img')
+//             dogImg.src = data.message
+//             dogImg.alt = "Random image of a dog"
+//             dogImg.width = 400;
+//             dogImageContainer.innerHTML = "" // clears previous image
+//             dogImageContainer.appendChild(dogImg)
+//         })
+//         .catch(error => console.log("Error fetching: ", error))
+// }
+
+// async function for random dog image
+async function getRandomDog() {
     const url = 'https://dog.ceo/api/breeds/image/random'
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const dogImg = document.createElement('img')
-            dogImg.src = data.message
-            dogImg.alt = "Random image of a dog"
-            dogImg.width = 400;
-            dogImageContainer.innerHTML = "" // clears previous image
-            dogImageContainer.appendChild(dogImg)
-        })
-        .catch(error => console.log("Error fetching: ", error))
+    try {
+        // get and parse data
+        const response = await fetch(url)
+        const data = await response.json()
+
+        // manipulate the DOM
+        const dogImg = document.createElement('img')
+        dogImg.src = data.message
+        dogImg.alt = "Random image of a dog"
+        dogImg.width = 400;
+        dogImageContainer.innerHTML = "" // clears previous image
+        dogImageContainer.appendChild(dogImg)
+    } catch (error) {
+        console.log("Error fetching: ", error)
+    }
 }
 
 newDogBtn.addEventListener('click', getRandomDog)

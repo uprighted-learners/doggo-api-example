8764f7cfbf07a07c1491533c5aeb207ca5bbbc2f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-21 18:53:42 -0500

    random dog image with promises

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+const dogImageContainer = document.getElementById('dog-image')
+const newDogBtn = document.getElementById('new-dog-btn')
+
+// function for a random dog image
+function getRandomDog() {
+    const url = 'https://dog.ceo/api/breeds/image/random'
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const dogImg = document.createElement('img')
+            dogImg.src = data.message
+            dogImg.alt = "Random image of a dog"
+            dogImg.width = 400;
+            dogImageContainer.innerHTML = "" // clears previous image
+            dogImageContainer.appendChild(dogImg)
+        })
+        .catch(error => console.log("Error fetching: ", error))
+}
+
+newDogBtn.addEventListener('click', getRandomDog)
+
+// get an initial image
+getRandomDog()
